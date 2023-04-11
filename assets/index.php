a4de7d32eb43255ccbba143b5905059b42d08d5d
Roberto Romero <roberto@arrenda.mx>
2023-04-11 17:30:02 -0600

    webpage

---
+++ assets/index.php
@@ -0,0 +1,61 @@
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group{
+border-radius:2px;
+-moz-border-radius:2;
+-webkit-border-radius:2;
+padding:10px;
+}
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.ui-state-highlight{
+background-color:#FCF8E3 !important;
+border-color:#FBEED5 !important;
+}
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group .control-label {
+color:#333333;
+font-family: Verdana, Geneva, sans-serif;
+font-size:14px;
+}
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.error .help-block,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.error .help-inline,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.error .help-block span.label {
+color:#FFFFFF;
+}
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group .label-important,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group .label-important .badge-important {
+background-color:#B94A48;
+}
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.error .help-block,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.error .help-inline,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group.error .help-block span.label {
+color:#FFFFFF;
+}
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group .controls input,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group .controls select,
+#jsn_form_1.jsn-master .jsn-bootstrap  .jsn-form-content .control-group .controls textarea {
+background:#ffffff;
+color:#666666;
+}
+.jsn-uniform.jsn-master .jsn-bootstrap .jsn-form-content .control-group {
+
+    border: 1px solid #fff;
+    margin-bottom: 11px;
+    padding: 10px;
+    transition: background 0.3s ease-out;
+    -moz-transition: background 0.3s ease-out;
+    -webkit-transition: background 0.3s ease-out;
+
+}
+
+#jsn_form_1.jsn-master .jsn-bootstrap .jsn-form-content .control-group .controls input, #jsn_form_1.jsn-master .jsn-bootstrap .jsn-form-content .control-group .controls select, #jsn_form_1.jsn-master .jsn-bootstrap .jsn-form-content .control-group .controls textarea {
+
+     background-color:rgb(255,255,255, .6);
+   opacity:0.6;
+   filter:alpha(opacity=60);
+ }
+  #jsn_form_1.jsn-master .jsn-bootstrap 
+  .jsn-form-content .control-group 
+  .control-label {
+
+    color: #fff;
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 14px;
+
+}
